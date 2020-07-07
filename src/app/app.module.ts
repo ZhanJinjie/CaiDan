bdb0f66805ad6eae64be2cf6cdd4e73c90d890e3
zjj <1030101700@qq.com>
2020-07-07 13:43:28 +0800

    注释修改

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+/* 模块 */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http'; // 引入http和 jsonp（可处理跨域）
@@ -10,22 +11,28 @@ import { PcontentComponent } from './components/pcontent/pcontent.component';
 
 /* 服务 */
 import { CommonService } from './services/common.service'; // 接口服务
-import { HightlightDirective } from './directives/hightlight.directive';
+
+/* 指令 */
+import { HightlightDirective } from './directives/hightlight.directive'; // 属性指令--背景高亮
 
 @NgModule({
+  // 组件，指令 的声明
   declarations: [
     AppComponent,
     HomeComponent,
     PcontentComponent,
     HightlightDirective
   ],
+  // 导入其他模块，表示该模块依赖其他模块的组件
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     HttpClientJsonpModule
   ],
+  // 服务
   providers: [CommonService],
+  // 模块的启动组件，一般app模块才会用到
   bootstrap: [AppComponent]
 })
 export class AppModule { }
